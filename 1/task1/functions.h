a761a008af2e024573479f42fed27364448cecee
Boris Bozic <boris.bozic96@gmail.com>
2023-11-17 23:16:22 +0100

    Removed unused files

--- 1/task1/functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-void setTopThreelist(int (&a_topThreeList)[3], int &a_caloriesCarriedByElf);
-
-
-#endif // FUNCTIONS_H
--- 1/task1/types.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <string>
-std::string c_nameOfFile = "C:/Users/boris/OneDrive/Documents/Projects/adventOFCode/1/task1/inputFile.txt";
