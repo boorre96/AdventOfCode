aace42782c007f834865ef0dbb594235d42919a9
Boris Bozic <boris.bozic96@gmail.com>
2023-11-26 21:04:54 +0100

    First task done

---
+++ 4/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    string const inputFile = "C:/Users/boris/OneDrive/Documents/Projects/adventOFCode/4/pairsInput.txt";
+    ifstream assigmPairsInput(inputFile);
+    string line{}, pair11{}, pair22{};
+    pair <string, string> pair1, pair2;
+    int amountOftimes = 0;
+    while(assigmPairsInput.is_open())
+    {
+        getline(assigmPairsInput, line);
+        if(line.empty())
+            break;
+
+        pair11 = line.substr(0,line.find(','));
+        pair22 = line.substr(line.find(',')+1, line.size());
+
+        pair1.first = pair11.substr(0, pair11.find('-'));
+        pair1.second = pair11.substr(pair11.find('-')+1, pair11.size());
+
+
+        pair2.first = pair22.substr(0, pair22.find('-'));
+        pair2.second = pair22.substr(pair22.find('-')+1, pair22.size());
+
+        if(stoi(pair1.first) <= stoi(pair2.first) && stoi(pair1.second) >= stoi(pair2.second))
+            amountOftimes +=1;
+        else if(stoi(pair2.first) <= stoi(pair1.first) && stoi(pair2.second) >= stoi(pair1.second))
+            amountOftimes +=1;
+
+
+        pair11.clear();
+        pair22.clear();
+        cout << line << endl;
+
+    }
+    assigmPairsInput.close();
+    cout << amountOftimes << endl;
+    return 0;
+}
