3d4ad97f7dad48cf1071ac1f5e95cb45aa91b8c8
Boris Bozic <boris.bozic96@gmail.com>
2023-12-09 20:32:42 +0100

    Clean up AOC3 task 1

--- 2023/AOC3/main.cpp
+++ 2023/AOC3/main.cpp
@@ -4,30 +4,18 @@
 #include <algorithm>
 using namespace std;
 
-bool findSymblo(vector<vector<char>> const& a_partNumbers, int a_start, int a_end, int a_row)
+bool findSymbol(vector<vector<char>> const& a_partNumbers, int a_start, int a_end, int a_row)
 {
-    if(a_row == 0)
+    if(a_row == 0 || a_row == a_partNumbers.size()-1)
     {
+        int l = (a_row == 0) ? 1 : -1;
+
         for(int i = 0; i < a_partNumbers[a_row].size(); i+=1)
         {
             if(a_partNumbers[a_row][i] != '.' && !isdigit(a_partNumbers[a_row][i]))
                 if(i+1 == a_start || i-1 == a_end)
                     return true;
-            if(a_partNumbers[a_row+1][i] != '.' && !isdigit(a_partNumbers[a_row+1][i]))
-                if(i+1 >= a_start && i-1 <= a_end)
-                    return true;
-        }
-
-
-    }
-    else if(a_row == a_partNumbers.size()-1)
-    {
-        for(int i = 0; i < a_partNumbers[a_row].size(); i+=1)
-        {
-            if(a_partNumbers[a_row][i] != '.' && !isdigit(a_partNumbers[a_row][i]))
-                if(i+1 == a_start || i-1 == a_end)
-                    return true;
-            if(a_partNumbers[a_row-1][i] != '.' && !isdigit(a_partNumbers[a_row-1][i]))
+            if(a_partNumbers[a_row+l][i] != '.' && !isdigit(a_partNumbers[a_row+l][i]))
                 if(i+1 >= a_start && i-1 <= a_end)
                     return true;
         }
@@ -49,6 +37,7 @@ bool findSymblo(vector<vector<char>> const& a_partNumbers, int a_start, int a_en
     }
     return false;
 }
+
 int main()
 {
     /*
@@ -66,8 +55,10 @@ int main()
     //string const filename = "C:/Users/boris/OneDrive/Documents/Projects/adventOFCode/2023/AOC3/test.txt";
     string const filename = "C:/Users/boris/OneDrive/Documents/Projects/adventOFCode/2023/AOC3/AOC3.txt";
     ifstream partNumberGenerator(filename);
-    int sum = 0;
-    int startingPos{}, endingPos{};
+    int sum1 = 0;
+    int sum2 = 0;
+    int startingPos{}, endingPos{}, posSymb{};
+
     string number = {};
     while (partNumberGenerator.peek() != EOF)
     {
@@ -81,6 +72,15 @@ int main()
     partNumberGenerator.close();
     for(int i = 0; i < partNumbers.size(); i+=1)
         for(int j = 0; j <partNumbers[i].size(); j+= 1)
+        {
+            //            if(!isdigit(partNumbers[i][j]) && partNumbers[i][j] != '.')
+            //            {
+            //                posSymb = j;
+            //                if(findSymbol2(partNumbers, posSymb, i))
+            //                {
+            //                    cout << "Found task 2" << endl;
+            //                }
+            //            }
             if(isdigit(partNumbers[i][j]))
             {
                 startingPos = j;
@@ -90,15 +90,16 @@ int main()
                     j+=1;
                 }
                 endingPos = j-1;
-                if(findSymblo(partNumbers, startingPos, endingPos, i))
+                if(findSymbol(partNumbers, startingPos, endingPos, i))
                 {
                     cout << number << endl;
-                    sum+= stoi(number);
+                    sum1+= stoi(number);
                 }
                 number.clear();
-
             }
 
-    cout << sum << endl;
+        }
+
+    cout << sum1 << endl;
     return 0;
 }//529870 to high
